f084236b05c6cd3c7ecf92a5f9f7f44bc8d09ac6
Oscar Hernandez <oscarh425@outlook.com>
2022-01-13 09:51:13 -0800

    added server

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ if(process.argv[2] ===
         console.log('boooh!')
     }
 console.log(`the user is ${user}`)
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req,res) => {
+    res.json({
+        message: "hi this works"
+    })
+})
+
+app.listen(2000, () => {
+    console.log('listening on port 2000')
+})
