e769f070db2c48e30f8c6ad22f0f4419f8ab0c46
Oscar Hernandez <oscarh425@outlook.com>
2022-01-13 09:46:44 -0800

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const user = process.env.USER
+
+if(process.argv[2] === 
+    'web49'){
+        console.log('web 49 is the best')
+    } else {
+        console.log('boooh!')
+    }
+console.log(`the user is ${user}`)
